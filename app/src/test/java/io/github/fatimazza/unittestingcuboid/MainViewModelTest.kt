380a19219bd043a32ea153348c08563f831aea2a
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-08 18:52:06 +0700

    Unit Test - Add Test for Calculate Volume

--- app/src/test/java/io/github/fatimazza/unittestingcuboid/MainViewModelTest.kt
+++ app/src/test/java/io/github/fatimazza/unittestingcuboid/MainViewModelTest.kt
@@ -3,8 +3,25 @@ package io.github.fatimazza.unittestingcuboid
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.mockito.Mockito.mock
 
 class MainViewModelTest {
+
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var cuboidModel: CuboidModel
+
+    private val dummyHeight = 2.0
+    private val dummyLength = 3.0
+    private val dummyWidth = 4.0
+
+    private val dummyVolume = 24.0
+
+    @Before
+    fun before() {
+        cuboidModel = mock(CuboidModel::class.java)
+        mainViewModel = MainViewModel(cuboidModel)
+    }
 
     @Test
     fun getCircumference() {
@@ -16,6 +33,12 @@ class MainViewModelTest {
 
     @Test
     fun getVolume() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+        mainViewModel.save(dummyLength, dummyWidth, dummyHeight)
+
+        val volume = mainViewModel.getVolume()
+        assertEquals(dummyVolume, volume, 0.0001)
     }
 
     @Test
