5f1028bd30032795f698e7337decb454761126f8
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-08 18:27:57 +0700

    Cuboid Activity - Implement Calculation in OnClick Listener

--- app/src/main/java/io/github/fatimazza/unittestingcuboid/MainActivity.kt
+++ app/src/main/java/io/github/fatimazza/unittestingcuboid/MainActivity.kt
@@ -51,7 +51,52 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         btnSave.setOnClickListener(this)
     }
 
-    override fun onClick(view: View?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onClick(view: View) {
+        val height = etHeight.text.toString().trim()
+        val length = etLength.text.toString().trim()
+        val width = etWidth.text.toString().trim()
+
+        when {
+            height.isEmpty() -> etHeight.error = getString(R.string.empty_field)
+            length.isEmpty() -> etLength.error = getString(R.string.empty_field)
+            width.isEmpty() -> etWidth.error = getString(R.string.empty_field)
+            else -> {
+                val h = height.toDouble()
+                val l = length.toDouble()
+                val w = width.toDouble()
+                when {
+                    view.id == R.id.btn_calculate_circumference -> {
+                        tvResult.text = mainViewModel.getCircumference().toString()
+                        hideCalculationButton()
+                    }
+                    view.id == R.id.btn_calculate_surface_area -> {
+                        tvResult.text = mainViewModel.getSurfaceArea().toString()
+                        hideCalculationButton()
+                    }
+                    view.id == R.id.btn_calculate_volume -> {
+                        tvResult.text = mainViewModel.getVolume().toString()
+                        hideCalculationButton()
+                    }
+                    view.id == R.id.btn_save -> {
+                        mainViewModel.save(l, w, h)
+                        showCalculationButton()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showCalculationButton() {
+        btnCalculateCircumference.visibility = View.VISIBLE
+        btnCalculateSurfaceArea.visibility = View.VISIBLE
+        btnCalculateVolume.visibility = View.VISIBLE
+        btnSave.visibility = View.GONE
+    }
+
+    private fun hideCalculationButton() {
+        btnCalculateCircumference.visibility = View.GONE
+        btnCalculateSurfaceArea.visibility = View.GONE
+        btnCalculateVolume.visibility = View.GONE
+        btnSave.visibility = View.VISIBLE
     }
 }
