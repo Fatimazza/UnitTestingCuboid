c9fbaecc07133a900d85bd1d7a761cccb4a08780
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-08 19:05:46 +0700

    Unit Test - Add Test for Calculate Circumference, Surface Area

--- app/src/test/java/io/github/fatimazza/unittestingcuboid/MainViewModelTest.kt
+++ app/src/test/java/io/github/fatimazza/unittestingcuboid/MainViewModelTest.kt
@@ -4,8 +4,11 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class MainViewModelTest {
 
     private lateinit var mainViewModel: MainViewModel
@@ -15,6 +18,8 @@ class MainViewModelTest {
     private val dummyLength = 3.0
     private val dummyWidth = 4.0
 
+    private val dummyCircumference = 96.0
+    private val dummySurfaceArea = 52.0
     private val dummyVolume = 24.0
 
     @Before
@@ -25,10 +30,22 @@ class MainViewModelTest {
 
     @Test
     fun getCircumference() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+        mainViewModel.save(dummyLength, dummyWidth, dummyHeight)
+
+        val circumference = mainViewModel.getCircumference()
+        assertEquals(dummyCircumference, circumference, 0.0001)
     }
 
     @Test
     fun getSurfaceArea() {
+        cuboidModel = CuboidModel()
+        mainViewModel = MainViewModel(cuboidModel)
+        mainViewModel.save(dummyLength, dummyWidth, dummyHeight)
+
+        val surfaceArea = mainViewModel.getSurfaceArea()
+        assertEquals(dummySurfaceArea, surfaceArea, 0.0001)
     }
 
     @Test
@@ -39,9 +56,5 @@ class MainViewModelTest {
 
         val volume = mainViewModel.getVolume()
         assertEquals(dummyVolume, volume, 0.0001)
-    }
-
-    @Test
-    fun save() {
     }
 }
