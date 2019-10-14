cbf25e39b10d18123ea2272c031b284ce9fcead5
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-14 11:12:36 +0700

    Instrumentation Test - Add Test for calculate Circumference

--- app/src/androidTest/java/io/github/fatimazza/unittestingcuboid/MainActivityTest.kt
+++ app/src/androidTest/java/io/github/fatimazza/unittestingcuboid/MainActivityTest.kt
@@ -1,7 +1,42 @@
 package io.github.fatimazza.unittestingcuboid
 
-import org.junit.Assert.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
+    private val dummyCircumference = "96.0"
+    private val dummySurfaceArea = "52.0"
+    private val dummyVolume = "24.0"
+    private val dummyHeight = "2.0"
+    private val dummyLength = "3.0"
+    private val dummyWidth = "4.0"
+    private val emptyInput = ""
+    private val fieldEmpty = "Field ini tidak boleh kosong"
 
+    @get:Rule
+    var activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun getCircumference() {
+        onView(withId(R.id.edt_length)).perform(typeText(dummyLength), closeSoftKeyboard())
+        onView(withId(R.id.edt_width)).perform(typeText(dummyWidth), closeSoftKeyboard())
+        onView(withId(R.id.edt_height)).perform(typeText(dummyHeight), closeSoftKeyboard())
+
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_save)).perform(click())
+
+        onView(withId(R.id.btn_calculate_circumference)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_calculate_circumference)).perform(click())
+
+        onView(withId(R.id.tv_result)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_result)).check(matches(withText(dummyCircumference)))
+    }
 }
